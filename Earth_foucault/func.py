da72fd13f2eaa399f67e5575dc98d4c26137ab22
FrSh28 <37129124+FrSh28@users.noreply.github.com>
2019-04-16 20:30:55 +0800

    Update credit

--- Earth_foucault/func.py
+++ Earth_foucault/func.py
@@ -1,5 +1,5 @@
 '''
- Copyright (C) 2018 Yi-Fan Shyu, Yueh-Feng Ku
+ Copyright (C) 2019 Yi-Fan Shyu, Yueh-Feng Ku
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
